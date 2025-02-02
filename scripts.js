3da84eeeec799d015333f4be225220a67c729e11
LucasFTomazela <lucasfernandobts@gmail.com>
2025-02-02 15:27:46 -0300

    feat: adds number validation to input amount

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const amount = document.getElementById("amount");
+
+// Quero capturar o valor do input conforme a pessoa insere conteúdo
+amount.addEventListener("input", () => {
+
+  const hasCharectersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharectersRegex, "")
+})
