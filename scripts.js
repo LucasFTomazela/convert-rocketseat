87c64c84c9ae5263047bf986b30a6e2e0e5c4849
LucasFTomazela <lucasfernandobts@gmail.com>
2025-02-02 18:01:33 -0300

    feat: formatting total

--- scripts.js
+++ scripts.js
@@ -43,10 +43,19 @@ function convertCurrency(amount, price, symbol){
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
 
     // Calcula o total.
+    // let total = String(amount * price).replace(".", ",")
     let total = amount * price
 
+    // Verifica se o resultado não é um número.
+    if(isNaN(total)){
+      return alert("Por favor, digite o valor corretamente para converter")
+    }
+
+    // Formata o valor total.
+    total = formatCurrencyBRL(total).replace("R$", "")
+
     // Exibir o resultado total.
-    result.textContent = total
+    result.textContent = `${total} Reais`
 
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result")
