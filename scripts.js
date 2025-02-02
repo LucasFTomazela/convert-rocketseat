def924e9062a4f33f89c8327a6631c3e85664a9c
LucasFTomazela <lucasfernandobts@gmail.com>
2025-02-02 15:56:47 -0300

    feat: captured currency on submit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
-const amount = document.getElementById("amount");
+// Obtendo os elementos do formulário
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 // Quero capturar o valor do input conforme a pessoa insere conteúdo
 amount.addEventListener("input", () => {
@@ -6,3 +9,9 @@ amount.addEventListener("input", () => {
   const hasCharectersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharectersRegex, "")
 })
+
+// Captura o evento de submit(enviar) do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault()
+  console.log(currency.value)
+}
