109ec38387c93da915db000823d50be199d3d255
LucasFTomazela <lucasfernandobts@gmail.com>
2025-02-02 17:26:02 -0300

    feat: displaying currency quote

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 
 // Quero capturar o valor do input conforme a pessoa insere conteúdo
 amount.addEventListener("input", () => {
@@ -37,8 +38,11 @@ form.onsubmit = (event) => {
 // Função para converter a moeda
 function convertCurrency(amount, price, symbol){
   try {
+    description.textContent = `${symbol} 1 = R$ ${price}`
+
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result")
+    
   } catch (error) {
     // Remove a classe do footer removendo ele da tela.
     footer.classList.remove("show-result")
